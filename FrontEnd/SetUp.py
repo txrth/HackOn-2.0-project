a7193c9676efa15592ed69dab3e7c46a8e1deb55
txrth <4tirth.patel@gmail.com>
2021-05-28 13:41:51 -0400

    test

---
+++ FrontEnd/SetUp.py
@@ -0,0 +1,6 @@
+
+class set_up:
+
+
+    def __init__(self):
+        print("hello")
--- RunnerClass.py
+++ RunnerClass.py
@@ -1,10 +1,14 @@
 import FrontEnd.SetUp
 
+class Main:
 
-def setup():
-    set = FrontEnd.SetUp
+    def setup(self):
+        set = FrontEnd.SetUp.set_up()
+
+    def __init__(self):
+        self.setup()
 
 
 
 if __name__ == '__main__':
-    setup()
+    Main()
